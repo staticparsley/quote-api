2e542715f69e5b0ba63b095361975e0de8bc22c3
staticparsley <rubenlmarin92@gmail.com>
2026-07-07 02:03:58 -0400

    added metrics

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import quoteRoutes from "./routes/quoteRoutes.js";
 import { renderHomePage } from "./views/homeView.js";
 import { getHealth, getVersion } from "./controllers/systemController.js";
+import { getMetrics } from "./metrics/prometheus.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(getMetrics);
 
 app.get("/", (req, res) => {
   res.type("html").send(renderHomePage());
@@ -13,6 +15,7 @@ app.get("/", (req, res) => {
 
 app.get("/health", getHealth);
 app.get("/version", getVersion);
+app.get("/metrics", getMetrics);
 
 app.use(quoteRoutes);
 
---
+++ src/metrics/prometheus.js
@@ -0,0 +1,33 @@
+import client from "prom-client";
+
+export const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+// export const httpRequestDurationSeconds = new client.Histogram({
+//   name: "http_request_duration_seconds",
+//   help: "Duration of HTTP requests in seconds",
+//   labelNames: ["method", "route", "status_code"],
+//   buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5],
+// });
+
+// register.registerMetric(httpRequestDurationSeconds);
+
+// export function metricsMiddleware(req, res, next) {
+//   const end = httpRequestDurationSeconds.startTimer();
+
+//   res.on("finish", () => {
+//     end({
+//       method: req.method,
+//       route: req.route?.path || req.path,
+//       status_code: res.statusCode,
+//     });
+//   });
+
+//   next();
+// }
+
+export async function getMetrics(req, res) {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+}
